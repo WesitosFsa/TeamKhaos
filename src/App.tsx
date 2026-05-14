bc5254ed699c9e8962d738a2f1da288c079173ca
WesitosFsa <mateo.garzon@epn.edu.ec>
2026-05-13 23:50:20 -0500

    Add VisitorHome route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import MainLayout from './layout/MainLayout';
 import Home from './pages/Home';
 import Intro from './pages/Intro';
+import VisitorHome from './pages/VisitorHome';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Intro />} />
         <Route element={<MainLayout />}>
           <Route path="/home" element={<Home />} />
+          <Route path="/visitante" element={<VisitorHome />} />
         </Route>
       </Routes>
     </BrowserRouter>
